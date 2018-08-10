2c130d1365d24f5771897c5a09e02a33657aeff1
Carl Hain <crhain@gmail.com>
2018-08-10 16:23:53 -0500

    Create ComicBook and Context classes; write simple demo that adds a comic book to database and then lists it to console.

---
+++ src/ComicBookGalleryModel/ComicBookGalleryModel/Context.cs
@@ -0,0 +1,17 @@
+﻿using ComicBookGalleryModel.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComicBookGalleryModel
+{
+    //This class interacts with Entity Framework.
+    public class Context : DbContext
+    {
+        //add a DbSet property for each model in the database to create a table for that model
+        public DbSet<ComicBook> ComicBooks { get; set; }
+    }
+}
--- src/ComicBookGalleryModel/ComicBookGalleryModel/Program.cs
+++ src/ComicBookGalleryModel/ComicBookGalleryModel/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ComicBookGalleryModel.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,30 @@ namespace ComicBookGalleryModel
     {
         static void Main(string[] args)
         {
+            //must use a 'using' statement because Context inheriets from a class that implements IDisposable
+            // and we need to release memory when done using it.  'using' statement is the easiest way to do this.
+            using(var context = new Context())
+            {
+                context.ComicBooks.Add(new ComicBook()
+                {
+                    SeriesTitle = "The Amazing Spider-Man",
+                    IssueNumber = 1,
+                    PublishedOn = DateTime.Today
+                });
+
+                context.SaveChanges();
+
+                var comicBooks = context.ComicBooks.ToList();
+
+                foreach(var comicBook in comicBooks)
+                {
+                    Console.WriteLine(comicBook.SeriesTitle);
+                }
+
+                Console.ReadLine();
+                
+            } 
+
         }
     }
 }
