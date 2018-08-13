8676acf72e2ab621dd5f85f2865af33699946a48
Carl Hain <crhain@gmail.com>
2018-08-13 15:26:47 -0500

    Add seed data.

--- src/ComicBookGalleryModel/ComicBookGalleryModel/Context.cs
+++ src/ComicBookGalleryModel/ComicBookGalleryModel/Context.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,7 +17,9 @@ namespace ComicBookGalleryModel
             //drops and initializes the database everytime the model changes
             //Database.SetInitializer(new DropCreateDatabaseIfModelChanges<Context>());
             //Database.SetInitializer(new CreateDatabaseIfNotExists<Context>());
-            Database.SetInitializer(new DropCreateDatabaseAlways<Context>());
+            //Database.SetInitializer(new DropCreateDatabaseAlways<Context>());
+
+            Database.SetInitializer(new DatabaseInitializer());
         }
 
         //add a DbSet property for each model in the database to create a table for that model
@@ -24,6 +27,18 @@ namespace ComicBookGalleryModel
 
         // can place a connection string as an argument to Context base class constructor or configure in in the App.config file.
         // We will use the App.config file, but syntax would look like
-        // public Context() : base(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=ComicBookGalleryConnectionString;Integrated Security=True;MultipleActiveResultSets=True"){}              
+        // public Context() : base(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=ComicBookGalleryConnectionString;Integrated Security=True;MultipleActiveResultSets=True"){}
+            
+        //override method on base class to change some conventions used by modelBuilder
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            //remove convention that uses plural names of classes to create tables
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+
+            //change decimal precision used by ComicBook.AverageRating
+            modelBuilder.Entity<ComicBook>()
+                        .Property(comicBook => comicBook.AverageRating)
+                        .HasPrecision(5, 2);
+        }
     }
 }
